b70845ec7d0ceb89aec1a03a590899f7d393dd07
scorpion00100 <danbetheliryivuze@gmail.com>
2023-01-10 13:16:48 +0100

    insert node as right_child

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,32 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_right - insert a node as the right-child of another node
+ * @parent: pointer to the node to insert the right-child
+ * @value: value to store in the new node
+ * Return: pointer to the new node, NULL on failure or if parent is NULL
+ */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node = NULL;
+
+	if (parent == NULL)
+		return (NULL);
+
+	new_node = binary_tree_node(parent, value);
+	if (new_node == NULL)
+		return (NULL);
+
+	if (parent->right == NULL)
+		parent->right = new_node;
+
+	else
+	{
+		/*parent->right = new_node;*/ /*moved down, because segmentation fault*/
+		new_node->right = parent->right;
+		parent->right = new_node;
+		new_node->right->parent = new_node;
+	}
+
+	return (new_node);
+}
