df4bda5c406c23641a747ca4aefd28c2109a4e78
scorpion00100 <danbetheliryivuze@gmail.com>
2023-01-12 05:35:02 +0100

    first test

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,7 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - Measure the height of a binary tree
+ * @tree: Pointer to the root node of the tree to measure the height
+ * Return: Height of the tree, if tree is NULL, return 0
+ */
